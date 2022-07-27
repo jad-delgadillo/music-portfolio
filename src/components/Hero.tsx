a1e2ba9861039798588ae1759cc53ab2df143c10
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-07-27 01:06:25 -0500

    kinda workin

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -50,7 +50,7 @@ export default function Hero() {
       leaveDelay: 100,
       height: 34,
       width: 34,
-      // display: "none",
+      display: "none",
     },
   };
   return (
@@ -63,16 +63,23 @@ export default function Hero() {
       <motion.div
         variants={variants}
         animate={"textLink"}
-        className="border border-black  h-[20px] w-[20px] rounded-full fixed top-0 left-0 pointer-events-none  "
+        className="border border-black  h-[50px] w-[50px] rounded-full fixed top-0 left-0 pointer-events-none  "
       ></motion.div>
 
       <div className=" flex flex-col min-h-screen rounded-[3.5rem] bg-white items-center justify-center">
         <h1
           onMouseEnter={textEnter}
           onMouseLeave={textLeave}
-          className=" filter md:text-[8vw] text-[14vw] font-bold mb-10 text-black hover:bg-white   hover:border pointer-events-auto px-10 rounded-full  transition-all  "
+          className=" filter md:text-[8vw] uppercase text-[14vw] font-black mb-10 text-black hover:bg-white   hover:border pointer-events-auto px-10 rounded-full  transition-all  "
         >
           alwaysjad
+        </h1>
+        <h1
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
+          className=" filter md:text-[3vw] text-[14vw] font-normal mb-10 text-black hover:bg-white   hover:border pointer-events-auto px-10 rounded-full  transition-all  "
+        >
+          dangerously good music.
         </h1>
       </div>
     </>
