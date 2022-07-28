e7a45f5490f86fcf8da351fb1e7b8c15be0c2a39
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-07-27 22:52:00 -0500

    kinda workin

--- src/pages/MusicProjects/CloverField.tsx
+++ src/pages/MusicProjects/CloverField.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import Button from "../../components/Button";
 import Container from "../../components/Container";
@@ -7,29 +8,45 @@ import Layout from "../../components/Layout";
 export default function CloverField() {
   const meta = {
     title: "Cloverfield Paradox",
+    company: "Netflix",
     description: "Super Bowl TV Spot",
+    credits: "Credits: Production, Compositon, Sound Design, Mixing and Master",
   };
   return (
     <div>
       <Layout>
-        <div className="block p-5">
-          {/* <Image
-            src={"/assets/trailers/cloverfield-paradox.jpg"}
-            width={400}
-            height={605}
-            layout="intrinsic"
-            className="rounded-lg"
-          /> */}
-          <video
-            src="/assets/videos/clover.mov"
-            controls
-            className="rounded-xl"
-          ></video>
-
-          <h1 className="text-lg font-medium">{meta.title}</h1>
-          <p className="font-extralight">{meta.description}</p>
+        <div className="lg:flex lg:flex-row flex flex-col items-center justify-center m-auto">
+          <div className="lg:flex lg:flex-row flex flex-col items-center justify-center m-auto p-5">
+            <div className="leading-tight lg:pr-[5vw] pr-0">
+              <h1 className="lg:text-[5.5vw] text-[12.5vw] font-medium">
+                {meta.title}
+              </h1>
+              <p className="lg:text-[3vw] text-[7.5vw] font-extralight">
+                {meta.company}
+              </p>
+              <p className="lg:text-[1.2vw] text-[4vw] font-extralight">
+                {meta.description}
+              </p>
+              <p className="lg:text-[0.9vw] text-[2.5vw] font-extralight">
+                {meta.credits}
+              </p>
+            </div>
+            <div className="lg:w-[40%] w-[100%] lg:mt-0 mt-10">
+              <video
+                src="/assets/videos/clover.mov"
+                controls
+                className="rounded-xl"
+              ></video>
+            </div>
+          </div>
         </div>
-        <Button>Projects</Button>
+        <div className="mb-10">
+          <Link href={"javascript:history.back()"}>
+            <a className="font-medium">
+              <Button>Back.</Button>
+            </a>
+          </Link>
+        </div>
       </Layout>
     </div>
   );
--- src/pages/MusicProjects/IsYou.tsx
+++ src/pages/MusicProjects/IsYou.tsx
@@ -1,32 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Button from "../../components/Button";
 import Container from "../../components/Container";
+import Layout from "../../components/Layout";
 
 export default function IsYou() {
   const meta = {
-    title: "All I See is You",
-    description: "Tonight Show with Jimmy Fallon",
+    title: "All I see is You",
+    company: "Tonight Show with Jimmy Fallon",
+    description: "National TV Broadcast",
+    credits: "Credits: Production, Compositon, Sound Design, Mixing and Master",
   };
   return (
     <div>
-      <Container>
-        <div className="block p-5">
-          {/* <Image
-            src={"/assets/trailers/alliseeisyou.jpg"}
-            width={400}
-            height={605}
-            layout="intrinsic"
-            className="rounded-lg"
-          /> */}
-          <video
-            src="/assets/videos/IsYou.mov"
-            controls
-            className="rounded-xl"
-          ></video>
-          <h1 className="text-lg font-medium">{meta.title}</h1>
-          <p className="font-extralight">{meta.description}</p>
+      <Layout>
+        <div className="lg:flex lg:flex-row flex flex-col items-center justify-center m-auto">
+          <div className="lg:flex lg:flex-row flex flex-col items-center justify-center m-auto p-5">
+            <div className="leading-tight lg:pr-[5vw] pr-0">
+              <h1 className="lg:text-[5.5vw] text-[12.5vw] font-medium">
+                {meta.title}
+              </h1>
+              <p className="lg:text-[3vw] text-[7.5vw] font-extralight">
+                {meta.company}
+              </p>
+              <p className="lg:text-[1.2vw] text-[4vw] font-extralight">
+                {meta.description}
+              </p>
+              <p className="lg:text-[0.9vw] text-[2.5vw] font-extralight">
+                {meta.credits}
+              </p>
+            </div>
+            <div className="lg:w-[40%] w-[100%] lg:mt-0 mt-10">
+              <video
+                src="/assets/videos/IsYou.mov"
+                controls
+                className="rounded-xl"
+              ></video>
+            </div>
+          </div>
         </div>
-      </Container>
+        <div className="mb-10">
+          <Link href={"javascript:history.back()"}>
+            <a className="font-medium">
+              <Button>Back.</Button>
+            </a>
+          </Link>
+        </div>
+      </Layout>
     </div>
   );
 }
