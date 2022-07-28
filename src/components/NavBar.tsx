9fa26013d3bc44ba4a964cc776e775af3a0719d0
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-07-27 19:28:52 -0500

    kinda workin

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,9 +24,9 @@ export default function NavBar() {
   };
 
   return (
-    <div className="relative m-auto items-center justify-center text-xl rounded-lg border-b w-screen bg-white   ">
+    <div className="relative m-auto items-center justify-center rounded-lg border-b w-screen bg-white   ">
       <div className="flex flex-row m-auto max-w-7xl justify-between w-screen p-5">
-        <h1 className="md:flex hidden text-2xl font-bold">alw.</h1>
+        <h1 className="md:flex hidden font-bold">alw.</h1>
 
         <nav className="flex flex-row md:m-0 m-auto ">
           {navLinks.map((nav) => renderNavLink(nav))}
